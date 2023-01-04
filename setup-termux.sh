98c4295dc7df2f96b766c52a7aaac4b9b8537bab
NyaaCaster <110224598+NyaaCaster@users.noreply.github.com>
2023-01-04 14:28:38 +0800

    Add files via upload

---
+++ setup-termux.sh
@@ -0,0 +1,18 @@
+echo "setup tools"
+pkg update -y
+pkg install python3 python3-pip -y
+python3 -m pip install --upgrade pip
+curl https://nyaacaster.github.io/Nyaa-Automata/requirements.txt -o requirements.txt
+pip3 install -r requirements.txt
+
+echo ""
+echo "git proxy"
+curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt -o proxy.txt
+
+echo ""
+echo "clone CC-attack"
+git clone https://github.com/Leeon123/CC-attack.git
+
+echo ""
+echo "HydaelynAttack"
+git clone https://github.com/NyaaCaster/HydaelynAttack.git
