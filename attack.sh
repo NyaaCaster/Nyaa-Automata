09a4a0724e854094f515bdb92704e61d9afbb377
NyaaCaster <110224598+NyaaCaster@users.noreply.github.com>
2023-01-04 13:55:20 +0800

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -4,23 +4,19 @@ python3 ~/Nyaa-Automata/CC-attack/cc.py -url http://fwe22.top/ -f ~/Nyaa-Automat
 echo ""
 echo "HydaelynAttack"
 python3 ~/Nyaa-Automata/HydaelynAttack/HydaelynAttack_p_once_noLog.py
-
 echo ""
 echo "updata proxy"
 curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt -o proxy.txt
-
 echo ""
 echo "updata HydaelynAttack"
 cd HydaelynAttack
 git pull
 cd ..
-
 echo ""
 echo "updata CC-attack"
 cd CC-attack
 git pull
 cd ..
-
 echo ""
 echo "Nyaa-Automata"
 git pull
