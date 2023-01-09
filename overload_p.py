f9feeeae351902711ccb01e849f1afc3368cf677
NyaaCaster <110224598+NyaaCaster@users.noreply.github.com>
2023-01-09 23:00:18 +0800

    Update overload_p.py

--- overload_p.py
+++ overload_p.py
@@ -30,7 +30,8 @@ def main() -> None:
         threads = 1
         sleep_time = check_number_input("sleep time") if "slowloris" in method else 0
         #target = "http://fwe22.top/"
-        target = "http://fwe88.top/"
+        #target = "http://fwe88.top/"
+        target = "http://fwe55.top/"
 
         with AttackMethod(
             duration=time,
