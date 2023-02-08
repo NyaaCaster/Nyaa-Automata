1ba64190146748de024370008bdb988e12ba5557
NyaaCaster <110224598+NyaaCaster@users.noreply.github.com>
2023-02-09 00:16:21 +0800

    Update setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -6,12 +6,12 @@ curl https://jsd.cdn.zzko.cn/gh/NyaaCaster/Nyaa-Automata@main/requirements.txt -
 pip3 install -r requirements.txt
 
 echo ""
-echo "git proxy"
-curl https://jsd.cdn.zzko.cn/gh/TheSpeedX/PROXY-List@master/http.txt -o proxy.txt
-
-echo ""
 echo "clone CC-attack"
 git clone https://hub.yzuu.cf/Leeon123/CC-attack.git
+cd CC-attack
+echo "git proxy"
+curl https://jsd.cdn.zzko.cn/gh/TheSpeedX/PROXY-List@master/http.txt -o proxy.txt
+cd ..
 
 echo ""
 echo "HydaelynAttack"
