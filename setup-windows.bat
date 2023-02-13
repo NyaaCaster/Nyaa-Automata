2f5ba935f7404e5c3cefa7c128d6149b268b390e
NyaaCaster <110224598+NyaaCaster@users.noreply.github.com>
2023-02-13 14:24:34 +0800

    Update setup-windows.bat

--- setup-windows.bat
+++ setup-windows.bat
@@ -1,20 +1,24 @@
 python3 -m pip install --upgrade pip
-curl https://nyaacaster.github.io/Nyaa-Automata/requirements.txt -o requirements.txt
+curl https://jsd.cdn.zzko.cn/gh/NyaaCaster/Nyaa-Automata@main/requirements.txt -o requirements.txt
 pip3 install -r requirements.txt
 
 echo ""
 echo "git proxy"
-curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt -o proxy.txt
+curl https://jsd.cdn.zzko.cn/gh/TheSpeedX/PROXY-List@master/http.txt -o proxy.txt
 
 echo ""
 echo "clone CC-attack"
-git clone https://github.com/Leeon123/CC-attack.git
+git clone https://hub.yzuu.cf/Leeon123/CC-attack.git
+cd CC-attack
+echo "git proxy"
+curl https://jsd.cdn.zzko.cn/gh/TheSpeedX/PROXY-List@master/http.txt -o http.txt
+cd ..
 
 echo ""
 echo "HydaelynAttack"
-git clone https://github.com/NyaaCaster/HydaelynAttack.git
+git clone https://hub.yzuu.cf/NyaaCaster/HydaelynAttack.git
 
 echo ""
 echo "overload"
-git clone https://github.com/7zx/overload
-curl https://nyaacaster.github.io/Nyaa-Automata/overload_p.py -o overload/overload_p.py
+git clone https://hub.yzuu.cf/7zx/overload.git
+curl https://jsd.cdn.zzko.cn/gh/NyaaCaster/Nyaa-Automata@main/overload_p.py -o overload/overload_p.py
